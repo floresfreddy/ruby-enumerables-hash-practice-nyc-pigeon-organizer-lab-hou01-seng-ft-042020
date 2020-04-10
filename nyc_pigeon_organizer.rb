e9995b8d33171aad7c5c09ea6ed70d3244b9f75e
Freddy Flores <floresfreddy.ultra@gmail.com>
2020-04-10 05:52:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,7 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  count1 = 0 
-  while data[count] do
-    count2 = 0 
-    while data[count2] do 
+  new_pigeon = {}
+  data.each do 
       
     
 end
