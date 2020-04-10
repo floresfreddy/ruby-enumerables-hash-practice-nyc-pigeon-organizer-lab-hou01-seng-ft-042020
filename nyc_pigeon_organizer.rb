5bf7c32804876251eda3d509f1504a0f570a486f
Freddy Flores <floresfreddy.ultra@gmail.com>
2020-04-10 06:50:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
     attribute.each do |attribute_value, pigeon_name|
       pigeon_name.each do |name|
         new_pigeon[name] ||= {}
-        new_pigeon[name][attribute_type] = []
+        new_pigeon[name][attribute_type] ||= []
         new_pigeon[name][attribute_type].push(attribute_value.to_s)
       end
     end
